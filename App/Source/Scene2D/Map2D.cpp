d98e6a23e24655be1eba3bf9f9cd8a73156d72b2
Muhd Haidil <204122U@mymail.nyp.edu.sg>
2021-08-18 10:42:23 +0800

    .

--- App/Source/Scene2D/Map2D.cpp
+++ App/Source/Scene2D/Map2D.cpp
@@ -166,6 +166,7 @@ bool CMap2D::Init(	const unsigned int uiNumLevels,
 		return false;
 	}
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 	if (LoadTexture("Image/scifidoor2.tga", 13) == false)
 	{
@@ -174,12 +175,17 @@ bool CMap2D::Init(	const unsigned int uiNumLevels,
 	}
 
 =======
+=======
+>>>>>>> 0f5613b523d878f6c53d36be83a70cf6eca8e760
 	//load down lift switch texture
 	if (LoadTexture("Image/Scene2D_liftSwitch.png", 13) == false)
 	{
 		std::cout << "Failed to load liftswitch texture" << std::endl;
 		return false;
 	}
+<<<<<<< HEAD
+>>>>>>> 0f5613b523d878f6c53d36be83a70cf6eca8e760
+=======
 >>>>>>> 0f5613b523d878f6c53d36be83a70cf6eca8e760
 	// 100 - 199 : Collidable Objects
 	if (LoadTexture("Image/tile.tga", 100) == false) //Original: 1
@@ -387,8 +393,11 @@ bool CMap2D::Init(	const unsigned int uiNumLevels,
 		std::cout << "Failed to load up lift texture" << std::endl;
 		return false;
 	}
+<<<<<<< HEAD
 
 	
+=======
+>>>>>>> 0f5613b523d878f6c53d36be83a70cf6eca8e760
 
 	// Initialise the variables for AStar
 	m_weight = 1;
--- App/Source/Scene2D/Scene2D.cpp
+++ App/Source/Scene2D/Scene2D.cpp
@@ -123,9 +123,12 @@ bool CScene2D::Init(void)
 	}
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 	//Level 1
 	if (cMap2D->LoadMap("Maps/DM2213_Map_Level_Test_Level.csv") == false)
 =======
+=======
+>>>>>>> 0f5613b523d878f6c53d36be83a70cf6eca8e760
 	// Load the map into an array
 	if (cMap2D->LoadMap("Maps/DM2213_Map_Level_Test.csv") == false)
 	{
@@ -133,6 +136,7 @@ bool CScene2D::Init(void)
 		return false;
 	}
 	if (cMap2D->LoadMap("Maps/DM2213_Map_Level_Test2.csv", 1) == false)
+<<<<<<< HEAD
 >>>>>>> 0f5613b523d878f6c53d36be83a70cf6eca8e760
 	{
 		// The loading of a map has failed. Return false
@@ -141,6 +145,8 @@ bool CScene2D::Init(void)
 
 	//Level 2
 	if (cMap2D->LoadMap("Maps/DM2213_Map_Level_Test2.csv", 1) == false)
+=======
+>>>>>>> 0f5613b523d878f6c53d36be83a70cf6eca8e760
 	{
 		// The loading of a map has failed. Return false
 		return false;
