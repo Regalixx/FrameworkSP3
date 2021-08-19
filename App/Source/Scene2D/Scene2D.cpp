d5378c1d797ad1384180905ef630a9df3d5edad6
Emotionnss <benchng123@gmail.com>
2021-08-19 14:55:56 +0800

    FIXED EVERYTHING HEHE

--- App/Source/Scene2D/Scene2D.cpp
+++ App/Source/Scene2D/Scene2D.cpp
@@ -209,7 +209,7 @@ bool CScene2D::Init(void)
 
 
 	// Load the map into an array
-	if (cMap2D->LoadMap("Maps/DM2213_Map_Level_Test_Level.csv") == false) //Changed
+	if (cMap2D->LoadMap("Maps/DM2213_Map_Level_Test.csv") == false) //Changed
 	{
 		// The loading of a map has failed. Return false
 		return false;
